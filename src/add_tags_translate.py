261e05fd6d56b50901b085730fbd88e754c7840e
NickStrain <shugavaneshwar13@gmail.com>
2024-02-13 21:48:14 +0530

    update

---
+++ src/add_tags_translate.py
@@ -0,0 +1,17 @@
+from tqdm import tqdm
+
+def add_token(sent:str,src_lang:str,tgt_lang:str,delimiter:str =" " ) -> str:
+    
+    
+    return src_lang+delimiter+tgt_lang+delimiter+sent
+
+
+if __name__ == "__main__":
+    infname =
+    outfname = 
+    scr_lang = 
+    tgt_lang = 
+    
+    
+    with open(infname,"r",encoding="utf-8") as infname 
+    
